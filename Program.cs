ccd1df3cceaae34d00483ecbe34374547b770f5d
joyez <joyezz333@gmail.com>
2023-01-24 21:22:21 +0300

    добавил задачу

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = {"GeekBrains","0","helloworld"};
+string[] newArray = new string[array.Length];
+SymbolArray(array,newArray);
+PrintArray(newArray);
+
+void SymbolArray(string[] array,string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
